6779e4d8534245bf7d8e7a26ba669170235c0117
ang <ang@Jervine-PC>
2014-06-01 15:18:28 +0800

    Edit Class name

---
+++ CDb.php
@@ -0,0 +1,38 @@
+<?php
+    class CDb {
+        private $host="localhost";
+        private $username="root";    // specify the sever details for mysql
+        private $password="@ngzy17171";
+        private $database="erp_db";
+        private $myconn;
+
+        function connectToDatabase(){
+
+            $conn= mysql_connect($this->host,$this->username,$this->password);
+
+            if(!$conn){
+                die ("Cannot connect to the database");
+            }else{
+                $this->myconn = $conn;
+            }
+
+            return $this->myconn;
+
+        }//connectToDatabase
+
+        function selectDatabase(){
+            mysql_select_db($this->database);  //use php inbuild functions for select database
+
+            if(mysql_error()){
+
+                echo "Cannot find the database ".$this->database;
+
+            }    
+        }//selectDatabase
+
+        function closeConnection(){
+            mysql_close($this->myconn);
+        }//closeConnection
+}
+?>
+
